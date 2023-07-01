f3a2875352bef42622686b4b105e9ee2a8b98b48
YR <93632404+mjyt@users.noreply.github.com>
2023-07-01 17:34:34 +0800

    Delete 0.install_eigen.sh

--- 0.install_eigen.sh
+++
@@ -1,9 +0,0 @@
-dir_abs_path=$(pwd)
-sudo rm -rf ${dir_abs_path}/eigen-3.4.0-install-x86/
-mkdir -p ${dir_abs_path}/eigen-3.4.0-install-x86/
-cd eigen-3.4.0/
-sudo rm -rf ./build/
-mkdir build
-cd build && cmake .. -DCMAKE_BUILD_TYPE=RELEASE -DCMAKE_INSTALL_PREFIX=${dir_abs_path}/eigen-3.4.0-install-x86
-make install
-
